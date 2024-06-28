039e1e7c820fc963aac55fa39ed7c7513fbf7d85
999.hyun_ <126471699+nicoleendgrasi@users.noreply.github.com>
2024-06-28 20:09:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,19 +12,22 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Paris,1900?",
-                afirmacao: "Incorreta"
+                afirmacao: "ERRADO!! Na verdade Em 6 de abril de 1896 começava em Atenas, na Grécia, a primeira edição dos Jogos Olímpicos da Era Moderna. O renascimento do espírito olímpico, interrompido no ano 392, deveu-se ao francês Barão de Coubertin."
+           "
             },
             {
                 texto: "Saint louis, 1904",
-                afirmacao: "Incorreta"
+                afirmacao: "ERRADO!! Na verdade Em 6 de abril de 1896 começava em Atenas, na Grécia, a primeira edição dos Jogos Olímpicos da Era Moderna. O renascimento do espírito olímpico, interrompido no ano 392, deveu-se ao francês Barão de Coubertin."
+           "
             },
             {
                  texto: "Atenas, 1896",
-                afirmacao: "Correta"
+                afirmacao: "Correta Em 6 de abril de 1896 começava em Atenas, na Grécia, a primeira edição dos Jogos Olímpicos da Era Moderna. O renascimento do espírito olímpico, interrompido no ano 392, deveu-se ao francês Barão de Coubertin."
+           "
             },
             {
                  texto: "Atenas, 1906",
-                afirmacao: "Incorreta"
+                afirmacao: " ERRADO!! Na verdade Em 6 de abril de 1896 começava em Atenas, na Grécia, a primeira edição dos Jogos Olímpicos da Era Moderna. O renascimento do espírito olímpico, interrompido no ano 392, deveu-se ao francês Barão de Coubertin."
             }
             
         ]
@@ -34,19 +37,19 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Paris Saint-Germain",
-                afirmacao: "Incorreta"
+                afirmacao: "Incorreta!! Na Verdade O marcante e muito conhecido apelido “Red Devils” se originou do rúgbi. Assim como o futebol, o esporte também é muito valorizado na Inglaterra. Por volta dos anos 1930, a melhor equipe de rúgbi inglesa era de Salford, região metropolitana de Manchester. Manchester United – Red Devils."
             },
             {
                 texto: "Manchester United",
-                afirmacao: "Correta"
+                afirmacao: "Correta!! O marcante e muito conhecido apelido “Red Devils” se originou do rúgbi. Assim como o futebol, o esporte também é muito valorizado na Inglaterra. Por volta dos anos 1930, a melhor equipe de rúgbi inglesa era de Salford, região metropolitana de Manchester. Manchester United – Red Devils."
             },
             {
                texto: "Barcelona",
-                afirmacao: "Incorreta"
+                afirmacao: "Incorreta!! Na Verdade O marcante e muito conhecido apelido “Red Devils” se originou do rúgbi. Assim como o futebol, o esporte também é muito valorizado na Inglaterra. Por volta dos anos 1930, a melhor equipe de rúgbi inglesa era de Salford, região metropolitana de Manchester. Manchester United – Red Devils."
             },
             {
                 texto: "Liverpool",
-                afirmacao: "Incorreta"  
+                afirmacao: "Incorreta!! Na verdade O marcante e muito conhecido apelido “Red Devils” se originou do rúgbi. Assim como o futebol, o esporte também é muito valorizado na Inglaterra. Por volta dos anos 1930, a melhor equipe de rúgbi inglesa era de Salford, região metropolitana de Manchester. Manchester United – Red Devils."  
             }
         ]
     },
@@ -54,20 +57,20 @@ const perguntas = [
         enunciado: "Qual motorista ganhou mais campeonatos de fórmula 1?",
         alternativas: [
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Max Verstappen",
+                afirmacao: "Correto!! O neerlandês Max Verstappen, é o atual campeão da categoria, pela Red Bull. Em um total de 74 temporadas, 34 pilotos conquistaram o título mundial de pilotos, sendo o alemão Michael Schumacher e o inglês Lewis Hamilton, os maiores vencedores com 7 títulos cada um"
             },
             {
-                texto: "",
-                afirmacao: "  "
+                texto: "Lewis Hamilton",
+                afirmacao: "Incorreto na verdade  O neerlandês Max Verstappen, é o atual campeão da categoria, pela Red Bull. Em um total de 74 temporadas, 34 pilotos conquistaram o título mundial de pilotos, sendo o alemão Michael Schumacher e o inglês Lewis Hamilton, os maiores vencedores com 7 títulos cada um "
             },
             {
-                texto: "",
-                afirmacao: "" 
+                texto: "Ayrton Senna",
+                afirmacao: "Incorreta na verdade O neerlandês Max Verstappen, é o atual campeão da categoria, pela Red Bull. Em um total de 74 temporadas, 34 pilotos conquistaram o título mundial de pilotos, sendo o alemão Michael Schumacher e o inglês Lewis Hamilton, os maiores vencedores com 7 títulos cada um" 
             },
             {
-                texto: "",
-                afirmacao: " "
+                texto: "George Russell",
+                afirmacao: "Incorreto na verdade O neerlandês Max Verstappen, é o atual campeão da categoria, pela Red Bull. Em um total de 74 temporadas, 34 pilotos conquistaram o título mundial de pilotos, sendo o alemão Michael Schumacher e o inglês Lewis Hamilton, os maiores vencedores com 7 títulos cada um "
             }
         ]
     },
